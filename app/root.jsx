690145e7a74890fa8e571d83a7ab17cbff83adad
UrielMartinez21 <uriellmartinezz35@gmail.com>
2023-01-01 18:48:47 -0600

    Agregando estilos css

--- app/root.jsx
+++ app/root.jsx
@@ -1,3 +1,44 @@
+import { Meta, Links } from "@remix-run/react"
+import styles from './styles/index.css'           // Se cargara en todas las paginas
+
+//---> Funcion para el componente importado 'Meta'
+//---> Añadir informacion a la pagina
+export function meta() {
+  return (
+    {
+      charset: 'utf-8',
+      title: 'GuitarLa-Remix',
+      viewport:'width=device-width,initial-scale=1'
+    }
+  )
+}
+
+//---> Funcion para el componente importado 'Links'
+export function links() {
+  return [
+    {
+      rel: 'stylesheet',
+      href: 'https://necolas.github.io/normalize.css/8.0.1/normalize.css'
+    },
+    {
+      rel: 'stylesheet',
+      href: styles
+    },
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.googleapis.com'
+    },    {
+      rel: 'preconnect',
+      href: 'https://fonts.gstatic.com',
+      crossOrigin: 'true'
+    },    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Outfit:wght@400;700;900&display=swap'
+    }      
+  ]
+}
+
+//---> Componente principal
 export default function App() {
   return (
     <Document>
@@ -6,11 +47,13 @@ export default function App() {
   )
 }
 
+//---> Componente para darle formato a la pagina
 function Document({ children }) {
   return (
     <html lang="es">
       <head>
-        <title>GuitarLA-Remix</title>
+        <Meta />
+        <Links />
       </head>
       <body>
         {children}
